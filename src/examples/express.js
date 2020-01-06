1020c0e8c80caf417c7c70eb9a85f9847a37c829
song <song.teng@gmail.com>
2020-01-06 05:46:41 +0000

    Minor update

--- src/examples/express.js
+++ src/examples/express.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.use(express.static('/home/song_teng/sandboxes/qin'))
+app.use(express.static('/home/song_teng/projects/qin'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
