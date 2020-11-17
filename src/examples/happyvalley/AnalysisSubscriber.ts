f0e55ffe5bfc584d4cd0821b6eed2bd194963935
song <song.teng@gmail.com>
2020-11-17 14:15:54 +0800

    more generic analysis

--- src/examples/happyvalley/AnalysisSubscriber.ts
+++ src/examples/happyvalley/AnalysisSubscriber.ts
@@ -34,7 +34,6 @@ class RedisSubscriberCallback extends DefaultClientCallback {
 
     connectSuccess: (client: Client) => void = (client) => {
         this.client = client
-        console.log('connected!!')
         client.login(this.user, this.password)
     }
 
@@ -42,7 +41,7 @@ class RedisSubscriberCallback extends DefaultClientCallback {
     }
 
     loginSuccess: (sessionId: SessionId) => void  = sessionId => {
-        console.log('login success')
+        console.log(`login as ${this.user}`)
 
         if (this.cleanStart == 'yes') {
             this.client.removeAllRows(this.tableId)
@@ -53,12 +52,12 @@ class RedisSubscriberCallback extends DefaultClientCallback {
     }
 
     loginFailure: (errorCode: ErrorCode, reason: string) => void  = (errorCode, reason) => {
-        console.log(`login failure ${reason}`)
+        console.log(`login failure ${reason} as ${this.user}`)
         this.client.logout()
     }
 
     logoutSuccess: () => void = () => {
-        this.client.login('hv', 'hv')
+        this.client.login(this.user, this.password)
     }
 
     logoutFailure: (reason: string) => void = reason => {
@@ -103,16 +102,16 @@ const today =  `${year}${month}${day}`
 
 const redisHost = yargs.argv.redisHost ? yargs.argv.redisHost : 'localhost'
 const redisPort = yargs.argv.redisPort ? yargs.argv.redisPort : 6381
-const symbol = yargs.argv.symbol ? yargs.argv.symbol : 'HK.MHI2011'
-const tableId = yargs.argv.tableId ? yargs.argv.tableId : 'strategy_table_v8_id'
+// const symbol = yargs.argv.symbol ? yargs.argv.symbol : 'HK.MHI2011'
+const tableId = yargs.argv.tableId ? yargs.argv.tableId : 'strategy_table_v9_id'
 const user = yargs.argv.user ? yargs.argv.user : 'hv'
 const password = yargs.argv.password ? yargs.argv.password : 'hv'
-const strategy = yargs.argv.strategy ? yargs.argv.strategy : 'dawn'
+// const strategy = yargs.argv.strategy ? yargs.argv.strategy : 'dawn'
 const cleanStart = yargs.argv.cleanStart ? yargs.argv.cleanStart : 'no'
 const hanHost = yargs.argv.hanHost ? yargs.argv.hanHost : 'localhost'
 const hanPort = yargs.argv.hanPort ? yargs.argv.hanPort : 6383
 
-const channel = `strategy_${strategy}_${today}_analytics_channel`
+const channel = `strategy_${today}_analytics_channel`
 
 export class RedisSubscriber {
     private client = new Client(WebSocket)
@@ -134,46 +133,47 @@ const callback = (channel, message, client) => {
 
     const ts = payload[0]
     const content = payload[1]
+    const enabled = content?.enabled
+    const strategy = content?.strategy
 
-    const pnl = content?.risk?.pnl[symbol]
-    const position = content?.risk?.position[symbol]
+    // const pnl = content?.risk?.pnl[symbol]
+    // const position = content?.risk?.position[symbol]
+    //
+    // const future_price = content?.analysis?.future_price
+    // const index_price = content?.analysis?.index_price
+    // const future_open_price = content?.analysis?.future_open_price
+    // const future_price_at_stock_match_start = content?.analysis?.future_price_at_stock_match_start
+    // const future_price_at_stock_match_end = content?.analysis?.future_price_at_stock_match_end
+    // const index_future_spread = content?.analysis?.index_future_spread
+    // const future_price_moving_average = content?.analysis?.future_price_moving_average
+    // const future_price_lower_bound = content?.analysis?.future_price_lower_bound
+    // const future_price_upper_bound = content?.analysis?.future_price_upper_bound
+    //
+    // const future_return = content?.analysis?.future_return
+    // const future_return_moving_average = content?.analysis?.future_return_moving_average
+    // const future_return_lower_bound = content?.analysis?.future_return_lower_bound
+    // const future_return_upper_bound = content?.analysis?.future_return_upper_bound
+    //
+    // const trade_in_threshold = content?.analysis?.trade_in_threshold
+    // const take_profit = content?.analysis?.take_profit
+    // const cut_loss = content?.analysis?.cut_loss
 
-    const future_price = content?.analysis?.future_price
-    const index_price = content?.analysis?.index_price
-    const future_open_price = content?.analysis?.future_open_price
-    const future_price_at_stock_match_start = content?.analysis?.future_price_at_stock_match_start
-    const future_price_at_stock_match_end = content?.analysis?.future_price_at_stock_match_end
-    const index_future_spread = content?.analysis?.index_future_spread
-    const future_price_moving_average = content?.analysis?.future_price_moving_average
-    const future_price_lower_bound = content?.analysis?.future_price_lower_bound
-    const future_price_upper_bound = content?.analysis?.future_price_upper_bound
 
-    const future_return = content?.analysis?.future_return
-    const future_return_moving_average = content?.analysis?.future_return_moving_average
-    const future_return_lower_bound = content?.analysis?.future_return_lower_bound
-    const future_return_upper_bound = content?.analysis?.future_return_upper_bound
-
-    const trade_in_threshold = content?.analysis?.trade_in_threshold
-    const take_profit = content?.analysis?.take_profit
-    const cut_loss = content?.analysis?.cut_loss
-
-    const enabled = content?.analysis?.enabled
-
-    let trade_in_start_time = content?.analysis?.trade_in_time_interval_start
-    let trade_in_end_time = content?.analysis?.trade_in_time_interval_end
-    let force_trade_out_time = content?.analysis?.force_trade_out_time
+    // let trade_in_start_time = content?.analysis?.trade_in_time_interval_start
+    // let trade_in_end_time = content?.analysis?.trade_in_time_interval_end
+    // let force_trade_out_time = content?.analysis?.force_trade_out_time
 
     const rowId = uuid()
-    const values = [
-        strategy, enabled, pnl, position,
-        future_price, index_price, future_open_price, future_price_at_stock_match_end, index_future_spread,
-        future_price_moving_average, future_price_lower_bound, future_price_upper_bound,
-        future_return, future_return_moving_average, future_return_lower_bound, future_return_upper_bound,
-        trade_in_start_time, trade_in_end_time, force_trade_out_time, trade_in_threshold, take_profit, cut_loss, future_price_at_stock_match_start,
-        ts
-    ]
-
-    console.log(values)
+    // const values = [
+    //     strategy, enabled, pnl, position,
+    //     future_price, index_price, future_open_price, future_price_at_stock_match_end, index_future_spread,
+    //     future_price_moving_average, future_price_lower_bound, future_price_upper_bound,
+    //     future_return, future_return_moving_average, future_return_lower_bound, future_return_upper_bound,
+    //     trade_in_start_time, trade_in_end_time, force_trade_out_time, trade_in_threshold, take_profit, cut_loss, future_price_at_stock_match_start,
+    //     ts
+    // ]
+    const values = [strategy, enabled, JSON.stringify(content), ts]
+    // console.log(values)
 
     // console.log(values)
     client.appendRow(tableId, rowId, values)
